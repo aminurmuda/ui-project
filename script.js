ee35128abfb6a3eec6d54719d51851c4e5a7bc4a
aminurmuda <aminurmuda@gmail.com>
2018-08-02 08:24:56 +0700

    rename class tabcontent tablinks

--- script.js
+++ script.js
@@ -26,11 +26,11 @@ window.onclick = function(event) {
 
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
+    tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablinks");
+    tablinks = document.getElementsByClassName("tab-links");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
